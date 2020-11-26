e9a6ce83e2ecab492c78c7dfba23609477769931
Данила Суворов <dansuvtim@gmail.com>
2020-11-26 19:51:11 +0300

    change

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -12,4 +12,54 @@ namespace Stack
         {
         }
     }
+    class Stack
+    {
+        private String[] array;
+        private int size;
+
+        public Stack(String[] array) 
+        {
+            this.size = 1;
+            this.array = array;
+        }
+        public bool Empty()
+        {
+            if (size == 0) return false;
+            else return true;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+        public void PUSH(String something)
+        {
+            array.Push(something);
+        }
+        public String POP()
+        {
+            if (Empty() == true)
+            {
+                return(array.Pop);
+            } else return("error");
+        }
+        public String PEEK()
+        {
+            if (Empty() == true)
+            {
+                return (array.Peek);
+            } else return ("error");
+        }
+        public void CLEAR()
+        {
+            array.Clear();
+        }
+    }
 }
